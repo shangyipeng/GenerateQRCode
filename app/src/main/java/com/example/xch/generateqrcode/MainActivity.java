5f0f15e3dafe526c184a02ec66bd71c7a42ab0f0
yxc <xch_yang@126.com>
2018-09-05 11:41:07 +0800

    add logo and blcak_photo by takephoto or choose album.

--- app/src/main/java/com/example/xch/generateqrcode/MainActivity.java
+++ app/src/main/java/com/example/xch/generateqrcode/MainActivity.java
@@ -1,21 +1,49 @@
 package com.example.xch.generateqrcode;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentUris;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button btn_generate;
     private EditText et_content;
     private ImageView iv_qrcode;
-    private String content;
+    private Button btn_chooseLogo, btn_chooseblack;//选取logo，选取代替黑色色块的图片
+    private ImageView picture_logo, picture_black;
+    private String content;//二维码内容
+    public static final int TAKE_PHOTO = 1;//拍照
+    public static final int CHOOSE_PHOTO = 2;//从相册选择图片
+    private Uri imageUri;
+    private Bitmap logoBitmap;//logo图片
+    private Bitmap blackBitmap;//代替黑色色块的图片
+    private int remark;//标记返回的是logo还是代替黑色色块图片
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,24 +52,235 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         et_content = findViewById(R.id.et_content);
         btn_generate = findViewById(R.id.btn_generate);
         iv_qrcode = findViewById(R.id.iv_qrcode);
+        btn_chooseLogo = findViewById(R.id.btn_chooseLogo);
+        btn_chooseblack = findViewById(R.id.btn_chooseblack);
+        picture_logo = findViewById(R.id.picture_logo);
+        picture_black = findViewById(R.id.picture_black);
         btn_generate.setOnClickListener(this);
+        btn_chooseLogo.setOnClickListener(this);
+        btn_chooseblack.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_generate:
-                content=et_content.getText().toString();
-                Resources res = getResources();
-                Bitmap logoBitmap= BitmapFactory.decodeResource(res,R.mipmap.logo);
-                Bitmap blackBitmap= BitmapFactory.decodeResource(res,R.mipmap.bitmap_black2);
-
-                Bitmap mBitmap = QRCodeUtil.createQRCodeBitmap(content, 800, 800,"UTF-8",
-                        "H", "1", Color.BLACK, Color.WHITE,logoBitmap,0.2F,blackBitmap);
+                content = et_content.getText().toString();
+                Bitmap mBitmap = QRCodeUtil.createQRCodeBitmap(content, 800, 800, "UTF-8",
+                        "H", "1", Color.BLACK, Color.WHITE, logoBitmap, 0.2F, blackBitmap);
                 iv_qrcode.setImageBitmap(mBitmap);
+                break;
+            case R.id.btn_chooseLogo:
+                remark = 0;
+                ShowChooseDialog();
+                break;
+            case R.id.btn_chooseblack:
+                remark = 1;
+                ShowChooseDialog();
                 break;
             default:
                 break;
+        }
+    }
+
+    /**
+     * 弹出选择框
+     *
+     * @author xch
+     */
+    public void ShowChooseDialog() {
+        AlertDialog.Builder Choicebuilder = new AlertDialog.Builder(this);
+        Choicebuilder.setCancelable(false);
+        Choicebuilder
+                .setTitle("选择图片")
+                .setSingleChoiceItems(new String[]{"拍照上传", "从相册选择"}, -1,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                switch (which) {
+                                    case 0://拍照
+                                        takePhoto();
+                                        break;
+                                    case 1:// 从相册选择
+                                        choosePhotoFromAlbum();
+                                        break;
+                                    default:
+                                        break;
+                                }
+                                dialog.dismiss();
+                            }
+                        })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+        Choicebuilder.create();
+        Choicebuilder.show();
+    }
+
+
+    /**
+     * 拍照
+     */
+    private void takePhoto() {
+        // 创建File对象，用于存储拍照后的图片
+        File outputImage = new File(getExternalCacheDir(), "output_image.jpg");
+        try {
+            if (outputImage.exists()) {
+                outputImage.delete();
+            }
+            outputImage.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (Build.VERSION.SDK_INT < 24) {
+            imageUri = Uri.fromFile(outputImage);
+        } else {
+            imageUri = FileProvider.getUriForFile(MainActivity.this, "com.example.cameraalbumtest.fileprovider", outputImage);
+        }
+        // 启动相机程序
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, TAKE_PHOTO);
+    }
+
+    /**
+     * 从相册选取图片
+     */
+    private void choosePhotoFromAlbum() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        } else {
+            openAlbum();
+        }
+    }
+
+    /**
+     * /打开相册
+     */
+    private void openAlbum() {
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent, CHOOSE_PHOTO);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    openAlbum();
+                } else {
+                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case TAKE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    try {
+                        if (remark == 0) {//logo
+                            logoBitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                            // 将拍摄的照片显示出来
+                            picture_logo.setImageBitmap(logoBitmap);
+                        } else if (remark == 1) {//black
+                            blackBitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                            picture_black.setImageBitmap(blackBitmap);
+                        } else {
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            case CHOOSE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    // 判断手机系统版本号
+                    if (Build.VERSION.SDK_INT >= 19) {
+                        // 4.4及以上系统使用这个方法处理图片
+                        handleImageOnKitKat(data);
+                    } else {
+                        // 4.4以下系统使用这个方法处理图片
+                        handleImageBeforeKitKat(data);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @SuppressLint("NewApi")
+    private void handleImageOnKitKat(Intent data) {
+        String imagePath = null;
+        Uri uri = data.getData();
+        Log.d("TAG", "handleImageOnKitKat: uri is " + uri);
+        if (DocumentsContract.isDocumentUri(this, uri)) {
+            // 如果是document类型的Uri，则通过document id处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())) {
+                String id = docId.split(":")[1]; // 解析出数字格式的id
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+            } else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(docId));
+                imagePath = getImagePath(contentUri, null);
+            }
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            // 如果是content类型的Uri，则使用普通方式处理
+            imagePath = getImagePath(uri, null);
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            // 如果是file类型的Uri，直接获取图片路径即可
+            imagePath = uri.getPath();
+        }
+        displayImage(imagePath); // 根据图片路径显示图片
+    }
+
+    private void handleImageBeforeKitKat(Intent data) {
+        Uri uri = data.getData();
+        String imagePath = getImagePath(uri, null);
+        displayImage(imagePath);
+    }
+
+    private String getImagePath(Uri uri, String selection) {
+        String path = null;
+        // 通过Uri和selection来获取真实的图片路径
+        Cursor cursor = getContentResolver().query(uri, null, selection, null, null);
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+
+    /**
+     * 显示图片
+     *
+     * @param imagePath 图片路径
+     */
+    private void displayImage(String imagePath) {
+        if (imagePath != null) {
+
+            if (remark == 0) {//logo
+                logoBitmap = BitmapFactory.decodeFile(imagePath);
+                // 将拍摄的照片显示出来
+                picture_logo.setImageBitmap(logoBitmap);
+            } else if (remark == 1) {//black
+                blackBitmap = BitmapFactory.decodeFile(imagePath);
+                picture_black.setImageBitmap(blackBitmap);
+            } else {
+            }
+        } else {
+            Toast.makeText(this, "failed to get image", Toast.LENGTH_SHORT).show();
         }
     }
 }
